8cb1e12562aea1218ef7cc329d6c0431724eff43
Tevin Glover <tevwork.tg@gmail.com>
2023-10-31 18:14:38 -0400

    confused

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -10,7 +10,7 @@ public class LogginLab {
         this.threshold = 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// public Static mean you can pull from it and use it else where.
 
         logger.log(Level.INFO, "Hello World!");
         logger.log(Level.SEVERE, "Terrible Error!");
@@ -20,22 +20,27 @@ public class LogginLab {
 
     }
 
-    public Integer getThreshold() {
+    public Integer getThreshold() {// gets the threshold.
         return threshold;
     }
 
-    public void setThreshold(Integer threshold) {
+    public void setThreshold(Integer threshold) {  // this sets a string into an int.
         this.threshold = threshold;
     }
 
-    public boolean thresholdExceeds(Integer limit) {
-        if (this.threshold > limit) 
-          return true;
+    public boolean thresholdExceeds(Integer limit) { //this measures the int to the limit set for the limit.
+        if (this.threshold > limit) // if else statement.
+          return true; // this imply that it over the limit.
         else
-            return false;
+            return false; // this imply that it not over the limit.
     }
 
-    // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
+    /**Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
+    */
+    public void thresholdReached( ){
+        // need to get the threshold and
+    }
+
 }
